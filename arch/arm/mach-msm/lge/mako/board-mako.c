650c78edcf2dea3a5de3a9133b10db2e2e9303e8
houst0nn <houstonn@gmail.com>
2013-02-01 05:44:03 -0500

    Update arch/arm/mach-msm/lge/mako/board-mako.c
    geeb_nfc_back_u_go

--- arch/arm/mach-msm/lge/mako/board-mako.c
+++ arch/arm/mach-msm/lge/mako/board-mako.c
@@ -2090,6 +2090,9 @@ static void __init apq8064_mako_init(void)
 	lge_add_panic_handler_devices();
 	lge_add_backlight_devices();
 	lge_add_sound_devices();
+#if defined(CONFIG_LGE_NFC_PN544)
+        lge_add_nfc_devices();
+#endif
 	lge_add_bcm2079x_device();
 #ifdef CONFIG_LGE_QFPROM_INTERFACE
 	lge_add_qfprom_devices();
